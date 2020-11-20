6412353b4e4de0ce7b140726cb3aff684478c79b
Dmitriy Diukov <diukovdmitriy@gmail.com>
2020-11-20 16:42:14 +0200

    second task done

--- 2/script.js
+++ 2/script.js
@@ -3,7 +3,7 @@ const jsKnowledge = prompt('Знаете ли вы Javascript?');
 const htmlCssKnowledge = prompt('Знаете ли вы HTML и CSS?');
 const userExperiense = Number(prompt('Сколько лет составляет Ваш коммерческий опыт работы?'));
 
-if (userAge > 24 || jsKnowledge == 'да' || htmlCssKnowledge == 'да' || userExperiense > 0.5) {
+if (userAge > 24 && jsKnowledge == 'да' && htmlCssKnowledge == 'да' && userExperiense > 0.5) {
   alert('Поздравляем, вы приняты!');
 } else {
   alert('Извините, но Вы нам не подходите.')
