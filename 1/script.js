73858a38cd33d8f2a810e6417aa09429f22694aa
Dmitriy Diukov <diukovdmitriy@gmail.com>
2020-11-20 16:38:24 +0200

    first task supplemented

--- 1/script.js
+++ 1/script.js
@@ -1,14 +1,18 @@
-let date = new Date();
-let currentYear = date.getFullYear();
-let birthYear = prompt('Введите год рождения');
+const date = new Date();
+const currentYear = date.getFullYear();
+const birthYear = prompt('Введите год рождения');
 const userAge = currentYear - birthYear;
 
 if (isNaN(userAge) === true) {
   alert('Ты что, не знаешь цифр?');
 } else if(userAge > 17) {
-  alert('Доступ разрешен.')
+  alert('Доступ разрешен.');
 } else if (userAge < 12) {
   alert('Ты, мальчик, что здесь делаешь? А ну бегом к маме!');
 } else {
   alert('Доступ запрещен. Подрасти еще немного.');
 }
+
+let answer = isNaN(userAge) === true ? 'Ты что, не знаешь цифр?' : userAge > 17 ? 'Доступ разрешен.' : userAge < 12 ? 'Ты, мальчик, что здесь делаешь? А ну бегом к маме!' : 'Доступ запрещен. Подрасти еще немного.'
+
+alert(answer);
